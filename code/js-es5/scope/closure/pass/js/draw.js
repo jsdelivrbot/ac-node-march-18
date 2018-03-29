b51448f5e4400e071cc93fd0452d333207509987
Administrator <Administrator@ac-imaclon0029-2.local>
2018-03-29 13:21:48 +0100

    remove code

--- code/js-es5/scope/closure/pass/js/draw.js
+++
@@ -1,8 +0,0 @@
-
-function drawPass( message,gender ) {
-
-	var panel = $("<section>" , { class:"panel"}).appendTo('.people');
-	$("<img>" , { src: "media/" + gender + ".png"  }).appendTo( panel );
-	$("<p>",{ text:message } ).appendTo( panel );
-
-};
